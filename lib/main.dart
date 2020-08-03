b729cf4c09d956995ae3f3e406641908a76f5c51
Alan Cristian <lanzn16@outlook.com>
2020-08-03 20:36:24 -0300

    add home_tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vclothes_shop/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
+        title: 'VClothes Shop',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Color.fromARGB(255, 4, 125, 141)),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen());
   }
 }
